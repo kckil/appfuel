f8a24e0c4bd6672e5323045f981f6ef511848466
Robert Scott-Buccluech <rsb.code@gmail.com>
2012-07-05 12:12:01 -0700

    [bug fix] empty check should be null. Also change comment headers and removed hard tabs

--- package/Appfuel/Expr/BasicExpr.php
+++ package/Appfuel/Expr/BasicExpr.php
@@ -1,13 +1,9 @@
 <?php
 /**
  * Appfuel
- * PHP 5.3+ object oriented MVC framework supporting domain driven design. 
- *
- * @package     Appfuel
- * @author      Robert Scott-Buccleuch <rsb.code@gmail.com.com>
- * @copyright   2009-2010 Robert Scott-Buccleuch <rsb.code@gmail.com>
- * @license     http://www.apache.org/licenses/LICENSE-2.0
- */
+ * Copyright (c) Robert Scott-Buccleuch <rsb.appfuel@gmail.com>
+ * See LICENSE file at project root for details.
+ */ 
 namespace Appfuel\Expr;
 
 use InvalidArgumentException;
@@ -17,17 +13,17 @@ use InvalidArgumentException;
  */
 class BasicExpr implements ExprInterface
 {
-	/**
-	 * Operand used for this expression
-	 * @var string
-	 */
-	protected $operand = null;
+    /**
+     * Operand used for this expression
+     * @var string
+     */
+    protected $operand = null;
 
-	/**
-	 * Flag used to determine if the expr should be wrapped in parentheses
-	 * @var bool
-	 */
-	protected $isParentheses = false;
+    /**
+     * Flag used to determine if the expr should be wrapped in parentheses
+     * @var bool
+     */
+    protected $isParentheses = false;
 
     /**
      * @param   string   $operand
@@ -35,158 +31,158 @@ class BasicExpr implements ExprInterface
      */
     public function __construct($operand, $isParentheses = false)
     {
-		$this->setOperand($operand);
-		if (true === $isParentheses) {
-			$this->enableParentheses();
-		}
+        $this->setOperand($operand);
+        if (true === $isParentheses) {
+            $this->enableParentheses();
+        }
     }
 
-	/**
-	 * @reutrn	BasicExpr
-	 */
-	public function setParenthesesStatus($flag)
-	{
-		if (true === $flag) {
-			$this->isParentheses = true;
-		}
-		else {
-			$this->isParentheses = false;
+    /**
+     * @reutrn    BasicExpr
+     */
+    public function setParenthesesStatus($flag)
+    {
+        if (true === $flag) {
+            $this->isParentheses = true;
+        }
+        else {
+            $this->isParentheses = false;
 
-		}
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return	BasicExpr
-	 */
-	public function enableParentheses()
-	{
-		return $this->setParenthesesStatus(true);
-	}
+    /**
+     * @return    BasicExpr
+     */
+    public function enableParentheses()
+    {
+        return $this->setParenthesesStatus(true);
+    }
 
-	/**
-	 * @return	BasicExpr
-	 */
-	public function disableParentheses()
-	{
-		return $this->setParenthesesStatus(false);
-	}
+    /**
+     * @return    BasicExpr
+     */
+    public function disableParentheses()
+    {
+        return $this->setParenthesesStatus(false);
+    }
 
-	/**
-	 * @return	BasicExpr
-	 */
-	public function isParentheses()
-	{
-		return $this->isParentheses;
-	}
+    /**
+     * @return    BasicExpr
+     */
+    public function isParentheses()
+    {
+        return $this->isParentheses;
+    }
 
-	/**
-	 * @return	mixed string | object
-	 */
-	public function getOperand()
-	{
-		return	$this->operand;
-	}
+    /**
+     * @return    mixed string | object
+     */
+    public function getOperand()
+    {
+        return    $this->operand;
+    }
 
-	/**
-	 * return the string representation of the expression
-	 * 
-	 * @return	string
-	 */
-	public function build()
-	{
+    /**
+     * return the string representation of the expression
+     * 
+     * @return    string
+     */
+    public function build()
+    {
         $str = $this->doBuild();
         
-		if ($this->isParentheses()) {
+        if ($this->isParentheses()) {
             $str = "($str)";
         }
         return $str;
-	}
+    }
 
-	protected function doBuild()
-	{
-		return $this->convertToString($this->getOperand());
-	}
+    protected function doBuild()
+    {
+        return $this->convertToString($this->getOperand());
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString()
-	{
-		return $this->build();
-	}
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->build();
+    }
 
-	protected function convertToString($var)
-	{
-		if (is_scalar($var)) {
-			$var = (string)$var;
-		}
-		else if (is_object($var)) {
-			$var = $var->__toString();
-		}
-		else if (is_array($var)) {
-			$var = implode(',', $var);
-		}
+    protected function convertToString($var)
+    {
+        if (is_scalar($var)) {
+            $var = (string)$var;
+        }
+        else if (is_object($var)) {
+            $var = $var->__toString();
+        }
+        else if (is_array($var)) {
+            $var = implode(',', $var);
+        }
 
-		return $var;
-	}
+        return $var;
+    }
 
-	/**
-	 * @param	mixed	object | scalar
-	 * @return	null
-	 */
-	protected function setOperand($op)
-	{
-		if (! $this->isValid($op)) {
-			$err = 'Invalid operand must be scalar or object with __toString';
-			throw new InvalidArgumentException($err);
-		}
+    /**
+     * @param    mixed    object | scalar
+     * @return    null
+     */
+    protected function setOperand($op)
+    {
+        if (! $this->isValid($op)) {
+            $err = 'Invalid operand must be scalar or object with __toString';
+            throw new InvalidArgumentException($err);
+        }
 
-		$this->operand = $op;
-	}
+        $this->operand = $op;
+    }
 
-	/**
-	 * Both operator and operand share the same validation rule which is:
-	 * any no empty scalar value or any object that supports __toString
-	 *
-	 * @param	mixed	$op
-	 * @return	bool
-	 */
-	protected function isValid($op)
-	{		
-		if (empty($op)) {
-			return false;
-		}
+    /**
+     * Both operator and operand share the same validation rule which is:
+     * any no empty scalar value or any object that supports __toString
+     *
+     * @param    mixed    $op
+     * @return    bool
+     */
+    protected function isValid($op)
+    {        
+        if (null === $op) {
+            return false;
+        }
 
-		if (is_scalar($op)) {
-			return true;
-		}
+        if (is_scalar($op)) {
+            return true;
+        }
 
-		if (is_object($op) && method_exists($op, '__toString')) {
-			return true;
-		}
+        if (is_object($op) && method_exists($op, '__toString')) {
+            return true;
+        }
 
-		if (is_array($op)) {
-			foreach ($op as $item) {
-				if (empty($item)) {
-					return false;
-				}
+        if (is_array($op)) {
+            foreach ($op as $item) {
+                if (empty($item)) {
+                    return false;
+                }
 
-				if (is_scalar($item)) {
-					continue;
-				}
+                if (is_scalar($item)) {
+                    continue;
+                }
 
-				if (is_object($item) && method_exists($item, '__toString')) {
-					continue;
-				}
+                if (is_object($item) && method_exists($item, '__toString')) {
+                    continue;
+                }
 
-				return false;
-			}
-			return true;
-		}
+                return false;
+            }
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
 
--- package/Appfuel/Expr/UnaryExpr.php
+++ package/Appfuel/Expr/UnaryExpr.php
@@ -1,12 +1,8 @@
 <?php
 /**
  * Appfuel
- * PHP 5.3+ object oriented MVC framework supporting domain driven design. 
- *
- * @package     Appfuel
- * @author      Robert Scott-Buccleuch <rsb.code@gmail.com.com>
- * @copyright   2009-2010 Robert Scott-Buccleuch <rsb.code@gmail.com>
- * @license     http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright (c) Robert Scott-Buccleuch <rsb.appfuel@gmail.com>
+ * See LICENSE file at project root for details.
  */
 namespace Appfuel\Expr;
 
@@ -17,134 +13,134 @@ use InvalidArgumentException;
  */
 class UnaryExpr extends BasicExpr implements UnaryExprInterface
 {
-	/**
-	 * Operand used for this expression
-	 * @var string
-	 */
-	protected $operand = null;
+    /**
+     * Operand used for this expression
+     * @var string
+     */
+    protected $operand = null;
 
-	/**
-	 * Operator used in the expression
-	 * @var string
-	 */
-	protected $operator = null;
+    /**
+     * Operator used in the expression
+     * @var string
+     */
+    protected $operator = null;
 
-	/**
-	 * Determines if the expression is build as prexfix or postfix
-	 * @var string
-	 */
-	protected $fixType = 'pre';
+    /**
+     * Determines if the expression is build as prexfix or postfix
+     * @var string
+     */
+    protected $fixType = 'pre';
     
-	/**
+    /**
      * @param   string   $operand
      * @return  File
      */
     public function __construct($operator, $operand, $isParentheses = false)
     {
-		$this->setOperator($operator);
-		parent::__construct($operand, $isParentheses);
+        $this->setOperator($operator);
+        parent::__construct($operand, $isParentheses);
     }
 
-	/**
-	 * @param	string	$type	pre|post
-	 * @return	null
-	 */
-	public function setFixType($type)
-	{
-		$err = 'fix type must be (pre|post)';
-		if (! is_string($type) || empty($type)) {
-			throw new InvalidArgumentException(
-				"$err param given is empty or not a string"
-			);
-		}
+    /**
+     * @param    string    $type    pre|post
+     * @return    null
+     */
+    public function setFixType($type)
+    {
+        $err = 'fix type must be (pre|post)';
+        if (! is_string($type) || empty($type)) {
+            throw new InvalidArgumentException(
+                "$err param given is empty or not a string"
+            );
+        }
 
-		if (! in_array($type, array('pre', 'post'))) {
-			throw new InvalidArgumentException("$err param given is ($type)");
-		}
+        if (! in_array($type, array('pre', 'post'))) {
+            throw new InvalidArgumentException("$err param given is ($type)");
+        }
 
-		$this->fixType = $type;
-	}
+        $this->fixType = $type;
+    }
 
-	/**
-	 * @return	string
-	 */
-	public function getFixType()
-	{
-		return $this->fixType;	
-	}
+    /**
+     * @return    string
+     */
+    public function getFixType()
+    {
+        return $this->fixType;    
+    }
 
-	/**
-	 * @return	bool
-	 */
-	public function isPrefix()
-	{
-		return 'pre' === $this->fixType; 
-	}
+    /**
+     * @return    bool
+     */
+    public function isPrefix()
+    {
+        return 'pre' === $this->fixType; 
+    }
 
-	/**
-	 * @return	bool
-	 */
-	public function isPostfix()
-	{
-		return 'post' === $this->fixType; 
-	}
+    /**
+     * @return    bool
+     */
+    public function isPostfix()
+    {
+        return 'post' === $this->fixType; 
+    }
 
-	/**
-	 * @return	mixed string | object
-	 */
-	public function getOperator()
-	{
-		return $this->operator;
-	}
-	
-	/**
-	 * Build a postfix expression
-	 *
-	 * @return string
-	 */
-	public function buildPostfix()
-	{
-		$operand = $this->getOperand();
-		return  $this->convertToString($operand). ' ' . $this->getOperator();
-	}
+    /**
+     * @return    mixed string | object
+     */
+    public function getOperator()
+    {
+        return $this->operator;
+    }
+    
+    /**
+     * Build a postfix expression
+     *
+     * @return string
+     */
+    public function buildPostfix()
+    {
+        $operand = $this->getOperand();
+        return  $this->convertToString($operand). ' ' . $this->getOperator();
+    }
 
-	/**
-	 * Build a prefix expression
-	 *
-	 * @return string
-	 */
-	public function buildPrefix()
-	{
-		$operand = $this->getOperand();
-		return  $this->getOperator() . ' ' . $this->convertToString($operand);
-	}
+    /**
+     * Build a prefix expression
+     *
+     * @return string
+     */
+    public function buildPrefix()
+    {
+        $operand = $this->getOperand();
+        return  $this->getOperator() . ' ' . $this->convertToString($operand);
+    }
 
-	/**
-	 * @param	mixed	object | scalar
-	 * @return	null
-	 */
-	protected function setOperator($op)
-	{
-		if (empty($op) || ! is_string($op)) {
-			$err = 'Invalid operator must be a non empty string';
-			throw new InvalidArgumentException($err);
-		}
+    /**
+     * @param    mixed    object | scalar
+     * @return    null
+     */
+    protected function setOperator($op)
+    {
+        if (empty($op) || ! is_string($op)) {
+            $err = 'Invalid operator must be a non empty string';
+            throw new InvalidArgumentException($err);
+        }
 
-		$this->operator = $op;
-	}
+        $this->operator = $op;
+    }
 
-	/**
-	 * @return string
-	 */
-	protected function doBuild()
-	{
-		if ($this->isPrefix()) {
-			$str = $this->buildPrefix();
-		} 
-		else {
-			$str = $this->buildPostfix();
-		}
+    /**
+     * @return string
+     */
+    protected function doBuild()
+    {
+        if ($this->isPrefix()) {
+            $str = $this->buildPrefix();
+        } 
+        else {
+            $str = $this->buildPostfix();
+        }
 
-		return $str;
-	}
+        return $str;
+    }
 }
