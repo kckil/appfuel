0337621c709b135f0f793012afe8f3d186119494
Robert Scott-Buccluech <rsb.code@gmail.com>
2012-07-07 18:14:53 -0700

    [bug fix] TemplateInterface was missing a namespace declaration

--- package/Appfuel/View/TemplateInterface.php
+++ package/Appfuel/View/TemplateInterface.php
@@ -4,6 +4,8 @@
  * Copyright (c) Robert Scott-Buccleuch <rsb.appfuel@gmail.com>
  * See LICENSE file at project root for details.
  */
+namespace Appfuel\View;
+
 use RunTimeException,
     InvalidArgumentException,
     Appfuel\Html\Resource\PkgName,
