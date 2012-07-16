feca0c30338722b0827671e223e7cd697155ecf4
Robert Scott-Buccluech <rsb.code@gmail.com>
2012-07-16 14:46:43 -0700

    change license to mit to be more in line with symfony

---
+++ src/TestFuel/Component/Kernel/KernelTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Appfuel
+ * Copyright (c) Robert Scott-Buccleuch <rsb.appfuel@gmail.com>
+ * See LICENSE file at project root for details.
+ */
+namespace Appfuel\Tests\Component\Kernel;
+
+use StdClass,
+    PHPUnit_Framework_TestCase;
+
+class KernelTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @return null
+     */
+    public function mytest()
+    {
+        $this->assertTrue(true);
+    }
+}
