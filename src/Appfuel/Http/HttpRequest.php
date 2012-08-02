16f506036c8f8ed73a45967fe94d73b50557c206
Robert Scott-Buccluech <rsb.code@gmail.com>
2012-08-02 14:31:12 -0700

    adding server functionality to request object

--- src/Appfuel/Http/HttpRequest.php
+++ src/Appfuel/Http/HttpRequest.php
@@ -12,7 +12,9 @@ use LogicException,
     Appfuel\DataStructure\ArrayData;
 
 /**
- * This class was dervived from code of Symfony 2 
+ * This class was dervived from Symfony 2 code base 
+ * symfony/src/Symfony/Component/HttpFoundation/Request.php
+ * commit# 6ac8e7308dd3730c50662a70e635dccab1394560
  * 
  * Code is subject to MIT license
  * http://http://symfony.com/doc/current/contributing/code/license.html
@@ -38,10 +40,25 @@ class HttpRequest implements HttpRequestInterface
     protected $method = null;
 
     /**
-     * @var   string
+     * @var string
      */
     protected $requestUri = null;
-    
+
+    /**
+     * @var string
+     */ 
+    protected $baseUrl = null;
+
+    /**
+     * @var string
+     */ 
+    protected $basePath = null;
+ 
+    /**
+     * @var string
+     */ 
+    protected $pathInfo = null;
+   
     /**
      * @param   array   $params
      * @return  HttpRequest
@@ -75,6 +92,141 @@ class HttpRequest implements HttpRequestInterface
         return self::$isProxyTrusted;
     }
 
+    /**
+     * @param   string  $str
+     * @return  string
+     */
+    public static function normalizeQueryString($str)
+    {
+        if ('' === $str) {
+            return '';
+        }
+
+        $parts = array();
+        foreach(explode('&', $str) as $param) {
+            /*
+             * Ignore useless delimiters, e.g. "x=y&".
+             * Also ignore pairs with empty key, even if there was a value,
+             * e.g. "=value", as such nameless values can not be retrieved
+             */
+            if ('' === $param || '=' === $param[0]) {
+                continue;
+            }
+            $data = explode('=', $param, 2);
+            
+            /*
+             * GET parameters, are are submitted form a HTML form, encode spaces
+             * as "+" by default (as defined in enctype application/x-www-form
+             * -urlencoded). PHP also converts "+" to spaces when filling the
+             * global _GET or when using the parse_str. This is why we use
+             * urldecode and them normalize to RFC 3986 with rawurlencode
+             */
+            if (isset($data[1])) {
+                $parts[] = rawurlencode(urldecode($data[0])) . '=' .
+                           rawurlencode(urldecode($data[1]));
+            }
+            else {
+                $parts[] = rawurlencode(urldecode($data[0]));
+            }
+        }
+
+        return implode('&', $parts);
+    }
+
+    /**
+     * Returns a normailzed query string for the Request. It provides 
+     * consistent escaping. Unlike Symfony we do not sort the query string
+     *
+     * @return  string | null
+     */
+    public function getQueryString()
+    {
+        $str = static::normalizeQueryString($this->get('QUERY_STRING'));
+        return '' === $str ? null : $str;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getBaseUrl()
+    {
+        if (null === $this->baseUrl) {
+            $this->baseUrl = $this->prepareBaseUrl();
+        }
+
+        return $this->baseUrl;
+    }
+
+    /**
+     * Returns the root path from which this request is executed.
+     *
+     * Suppose that an index.php file instantiates this request object:
+     *
+     *  * http://localhost/index.php         returns an empty string
+     *  * http://localhost/index.php/page    returns an empty string
+     *  * http://localhost/web/index.php     returns '/web'
+     *  * http://localhost/we%20b/index.php  returns '/we%20b'
+     * 
+     * @return  string
+     */
+    public function getBasePath()
+    {
+        if (null === $this->basePath) {
+            $this->basePath = $this->prepareBasePath();
+        }
+
+        return $this->basePath;
+    }
+
+    /**
+     * Returns the path being requested relative to the executed script.
+     * (not urldecoded)
+     * 
+     * The path info always starts with a /.
+     *
+     * Suppose this request is instantiated from /mysite on localhost:
+     *
+     *  * http://localhost/mysite              returns an empty string
+     *  * http://localhost/mysite/about        returns '/about'
+     *  * htpp://localhost/mysite/enco%20ded   returns '/enco%20ded'
+     *  * http://localhost/mysite/about?var=1  returns '/about'
+     *
+     * @return string
+     */
+    public function getPathInfo()
+    {
+        if (null === $this->pathInfo) {
+            $this->pathInfo = $this->preparePathInfo();
+        }
+
+        return $this->pathInfo;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getRequestUri()
+    {
+        if (null !== $this->requestUri) {
+            return $this->requestUri;
+        }
+
+        $uri = '';
+        if ($this->exists('REQUEST_URI')) {
+            $uri = $this->get('REQUEST_URI');
+            $schemeAndHttpHost = $this->getSchemeAndHttpHost();
+            /* HTTP proxy reqs setup request uri with scheme and host 
+             * [and port] + the url path, only use url path
+             */
+            if (strpos($uri, $schemeAndHttpHost) === 0) {
+                $uri = substr($uri, strlen($schemeAndHttpHost));
+            }
+        }
+
+        $this->requestUri = $uri;
+        return $uri;
+    }
+
     /** 
      * @throws  LogicException 
      * @return  string 
@@ -104,6 +256,14 @@ class HttpRequest implements HttpRequestInterface
         $this->method = strtoupper($method);
 
         return $this->method;  
+    }
+
+    /**
+     * @return  bool
+     */
+    public function isMethodSafe()
+    {
+        return in_array($this->getMethod(), array('GET', 'HEAD'));
     }
 
     /**
@@ -166,6 +326,23 @@ class HttpRequest implements HttpRequestInterface
     {
         return $this->get('PHP_AUTH_PW');
     }
+
+    /**
+     * Returns a user name and, optionally, scheme specific information about
+     * how to gain authorization to access the server
+     *
+     * @return  string
+     */
+    public function getUserInfo()
+    {
+        $info = $this->getUser();
+        $pass = $this->getPassword();
+        if ('' != $pass) {
+            $info .= ":$pass";
+        }
+
+        return $info;
+    }
     
     /**
      * @return  string
@@ -188,6 +365,41 @@ class HttpRequest implements HttpRequestInterface
         $host = preg_replace('/:\d+$/', '', $host);
 
         return trim(strtolower($host));
+    }
+
+    /**
+     * Returns the HTTP host being requested, appending the port name if 
+     * its none standard.
+     *
+     * @return  string  
+     */
+    public function getHttpHost()
+    {
+        $scheme = $this->getScheme();
+        $port  = $this->getPort();
+
+        $host = $this->getHost();
+        if (('http' === $scheme && 80 == $port) || 
+            ('https' === $scheme && 443 == $port)) {
+            return $host;
+        }
+
+        return "$host:$port";
+    }
+
+    /**
+     * @return  string
+     */
+    public function getSchemeAndHttpHost()
+    {
+        $str = "{$this->getScheme()}://";
+        
+        $auth = $this->getUserInfo();
+        if ('' != $auth) {
+            $str .= "$auth@";
+        }
+
+        return "{$str}{$this->getHttpHost()}";
     }
 
     /**
@@ -301,5 +513,153 @@ class HttpRequest implements HttpRequestInterface
         }
 
         return sprintf($format, $ip);
+    }
+
+    protected function prepareBaseUrl()
+    {
+        $filename = basename($this->get('SCRIPT_FILENAME'));
+
+        if ($filename === basename($this->get('SCRIPT_NAME'))) {
+            $baseUrl = $this->get('SCRIPT_NAME');
+        }
+        else if ($filename === basename($this->get('PHP_SELF'))) {
+            $baseUrl = $this->get('PHP_SELF');
+        }
+        else if ($filename === basename($this->get('ORIG_SCRIPT_NAME'))) {
+            $baseUrl = $this->get('ORIG_SCRIPT_NAME');
+        }
+        else {
+            // backtrack up the script_filename to find the portion matching
+            $path = $this->get('PHP_SELF', '');
+            $file = $this->get('SCRIPT_FILENAME', '');
+            $segs = explode('/', trim($file, '/'));
+            $segs = array_reverse($segs);
+            $idx  = 0;
+            $last = count($segs);
+            $baseUrl = '';
+            do {
+                $seg = $segs[$idx];
+                $baseUrl = '/'. $seg . $baseUrl;
+                ++$idx;
+            } while (($last > $idx) && 
+                     (false !== ($pos = strpos($path, $baseUrl))) &&
+                     (0 != $pos));
+        }
+
+        // does the base url have anything in common with the request uri
+        $uri = $this->getRequestUri();
+        $prefix = $this->getUrlencodedPrefix($uri, $baseUrl);
+        if ($baseUrl && false !== $prefix) {
+            // full $baseUrl matches
+            return rtrim($prefix, '/');
+        }
+        
+        $prefix = $this->getUrlencodedPrefix($uri, dirname($baseUrl));
+        if ($baseUrl && false !== $prefix) {
+            // directory portion of the base url matches
+            return rtrim($prefix, '/');
+        }
+
+        $truncatedUri = $uri;
+        if (($pos = strpos($uri, '?')) !== false) {
+            $truncatedUri = substr($uri, 0, $pos);
+        }
+
+        $basename = basename($baseUrl);
+        if (empty($basename) || 
+            ! strpos(rawurldecode($truncatedUri), $basename)) {
+            // no match whatsoever set it to empty string
+            return '';
+        }
+
+       /*
+        * If using mod_rewrite or ISAPI_Rewrite strip the script filename
+        * out of the baseUrl. $pos !== 0 makes sure it in not matching
+        * a value from PATH_INFO or QUERY_STRING
+        */
+        if ((strlen($uri) >= strlen($baseUrl)) &&
+            ((false !== ($pos = strpos($uri, $baseUrl))) &&
+            ($post !== 0))) {
+            $baseUrl = substr($requestUri, 0, $pos + strlen($baseUrl));
+        }
+        
+        return rtrim($baseUrl, '/');
+    }
+
+    /**
+     * @return string
+     */
+    protected function prepareBasePath()
+    {
+        $filename = basename($this->get('SCRIPT_FILENAME'));
+        $baseUrl = $this->getBaseUrl();
+        if (empty($baseUrl)) {
+            return '';
+        }
+
+        if (basename($baseUrl) === $filename) {
+            $basePath = dirname($baseUrl);
+        } else {
+            $basePath = $baseUrl;
+        }
+
+        if ('\\' === DIRECTORY_SEPARATOR) {
+            $basePath = str_replace('\\', '/', $basePath);
+        }
+
+        return rtrim($basePath, '/');
+    }
+
+   /**
+     * @return string
+     */
+    protected function preparePathInfo()
+    {
+        $baseUrl = $this->getBaseUrl();
+
+        if (null === ($requestUri = $this->getRequestUri())) {
+            return '/';
+        }
+
+        $pathInfo = '/';
+
+        // Remove the query string from REQUEST_URI
+        if ($pos = strpos($requestUri, '?')) {
+            $requestUri = substr($requestUri, 0, $pos);
+        }
+
+        if ((null !== $baseUrl) && 
+            (false === ($pathInfo = substr($requestUri,strlen($baseUrl))))) {
+            // If substr() returns false PATH_INFO is set to an empty string
+            return '/';
+        } elseif (null === $baseUrl) {
+            return $requestUri;
+        }
+
+        return (string) $pathInfo;
+    }
+
+    /*
+     * Returns the prefix as encoded in the string when the string starts with
+     * the given prefix, false otherwise.
+     *
+     * @param string $string The urlencoded string
+     * @param string $prefix The prefix not encoded
+     *
+     * @return string|false The prefix as it is encoded in $string, or false
+     */
+    private function getUrlencodedPrefix($string, $prefix)
+    {
+        if (0 !== strpos(rawurldecode($string), $prefix)) {
+            return false;
+        }
+
+        $len = strlen($prefix);
+
+        if (preg_match("#^(%[[:xdigit:]]{2}|.){{$len}}#", $string, $match)) {
+            return $match[0];
+        }
+
+        return false;
     }
 }
--- src/TestFuel/Http/RequestTest.php
+++ src/TestFuel/Http/RequestTest.php
@@ -44,7 +44,131 @@ class HttpRequestTest extends FrameworkTestCase
             array('1')
         );
     }
- 
+
+    /**
+     * @return  array
+     */
+    public function provideIsMethodSafeData()
+    {
+        return array(
+            array('GET',    true),
+            array('HEAD',   true),
+            array('POST',   false),
+            array('PUT',    false),
+            array('DELETE', false)
+        );
+    }
+
+    /**
+     * @return  array
+     */
+    public function provideQueryStringData()
+    {
+        return array(
+            array('', '', 'empty string is safe'),
+            array('foo', 'foo', 'works with valueless params'),
+            array('foo=', 'foo=', 'includes dangling equal signs'),
+            array('foo=&bar=baz', 'foo=&bar=baz', 'works with empty params'),
+            array(
+                'him=John%20Doe&her=Jane+Doe',
+                'him=John%20Doe&her=Jane%20Doe', 
+                'normalize spaces in both encodings "%20" and "+"'
+            ),
+            array(
+                'foo[]=1&foo[]=2',
+                'foo%5B%5D=1&foo%5B%5D=2',
+                'allows array notation'
+            ),
+            array('foo=1&foo=2', 'foo=1&foo=2', 'allows repeated params'),
+            array(
+                'pa%3Dram=foo%26bar%3Dbaz&test=test',
+                'pa%3Dram=foo%26bar%3Dbaz&test=test',
+                'works with encoded delimiters'
+            ),
+            array('0', '0', 'allows "0"'),
+            array(
+                'Jane Doe&John%20Doe',
+                'Jane%20Doe&John%20Doe',
+                'normalizes encoding in keys'
+            ),
+            array(
+                'her=Jane Doe&him=John%20Doe',
+                'her=Jane%20Doe&him=John%20Doe',
+                'normalizes encoding in values'
+            ),
+            array('foo=bar&&&test&&', 'foo=bar&test', 'removes uneeded delims'),
+            array(
+                'formula=e=m*c^2',
+                'formula=e%3Dm%2Ac%5E2',
+                'correctly treats only the first "=" as delimiter and the next as value'
+            ),
+            array(
+                'foo=bar&=a=b&=x=y', 
+                'foo=bar', 
+                'removes params with empty key'
+            ),
+        );
+    }
+
+    /**
+     * @return  array
+     */
+    public function provideBaseUrlData()
+    {
+        return array(
+            array(
+                array(
+                    'REQUEST_URI'     =>  '/foo%20bar',
+                    'SCRIPT_FILENAME' => '/home/John Doe/public_html/foo bar/app.php',
+                    'SCRIPT_NAME'     => '/foo bar/app.php',
+                    'PHP_SELF'        => '/foo bar/app.php',
+                ),
+                '/foo%20bar',
+                '/',
+            ),
+            array(
+                array(
+                    'REQUEST_URI'     => '/foo%20bar/home',
+                    'SCRIPT_FILENAME' => '/home/John Doe/public_html/foo bar/app.php',
+                    'SCRIPT_NAME'     => '/foo bar/app.php',
+                    'PHP_SELF'        => '/foo bar/app.php',
+                ),
+                '/foo%20bar',
+                '/home',
+            ),
+            array(
+                array(
+                    'REQUEST_URI'     => '/foo%20bar/app.php/home',
+                    'SCRIPT_FILENAME' => '/home/John Doe/public_html/foo bar/app.php',
+                    'SCRIPT_NAME'     => '/foo bar/app.php',
+                    'PHP_SELF'        => '/foo bar/app.php',
+                ),
+                '/foo%20bar/app.php',
+                '/home',
+            ),
+            array(
+                array(
+                    'REQUEST_URI'     => '/foo%20bar/app.php/home%3Dbaz',
+                    'SCRIPT_FILENAME' => '/home/John Doe/public_html/foo bar/app.php',
+                    'SCRIPT_NAME'     => '/foo bar/app.php',
+                    'PHP_SELF'        => '/foo bar/app.php',
+                ),
+                '/foo%20bar/app.php',
+                '/home%3Dbaz',
+            ),
+            array(
+                array(
+                    'REQUEST_URI'     => '/foo/bar+baz',
+                    'SCRIPT_FILENAME' => '/home/John Doe/public_html/foo/app.php',
+                    'SCRIPT_NAME'     => '/foo/app.php',
+                    'PHP_SELF'        => '/foo/app.php',
+                ),
+                '/foo',
+                '/bar+baz',
+            ),
+        );
+    }
+
     /**
      * @test
      * @return  HttpRequest
@@ -150,6 +274,17 @@ class HttpRequestTest extends FrameworkTestCase
         $request = $this->createRequest($server);
 
         $result = $request->get($badKey);
+    }
+
+    /**
+     * @test
+     * @dataProvider    provideBaseUrlData
+     */
+    public function gettingBaseUrl($server, $expected, $expectedPathInfo)
+    {
+        $request = $this->createRequest($server);
+        $this->assertEquals($expected, $request->getBaseUrl());
+        $this->assertEquals($expectedPathInfo, $request->getPathInfo());
     }
 
     /**
@@ -321,7 +456,7 @@ class HttpRequestTest extends FrameworkTestCase
 
     /**
      * @test
-     * @depends creatingHttpRequest
+     * @depends gettingTheUser
      * @return  null
      */
     public function gettingThePassword()
@@ -334,6 +469,92 @@ class HttpRequestTest extends FrameworkTestCase
         $request = $this->createRequest($server);
         $this->assertNull($request->getPassword());
     }
+
+    /**
+     * @test
+     * @depends gettingThePassword
+     * @return  null
+     */
+    public function gettingUserInfo()
+    {
+        $request = $this->createRequest(array());
+        $this->assertEquals('', $request->getUserInfo());
+        
+        $server = array(
+            'PHP_AUTH_USER' => 'web-user'
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals('web-user', $request->getUserInfo());
+
+        $server = array(
+            'PHP_AUTH_USER' => 'web-user',
+            'PHP_AUTH_PW' => 'password',
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals('web-user:password', $request->getUserInfo());
+ 
+        $server = array(
+            'PHP_AUTH_USER' => 'web-user',
+            'PHP_AUTH_PW' => '',
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals('web-user', $request->getUserInfo());
+    }
+
+    /**
+     * @test
+     * @depends gettingUserInfo
+     * @return  null
+     */
+    public function gettingSchemeAndHttpHost()
+    {
+        $server = array(
+            'SERVER_NAME' => 'myserver',
+            'SERVER_PORT' => '90'
+        );
+        $request = $this->createRequest($server);
+        
+        $expected = 'http://myserver:90';
+        $this->assertEquals($expected, $request->getSchemeAndHttpHost());
+
+        $server['PHP_AUTH_USER'] = 'robert';
+        $expected = 'http://robert@myserver:90';
+        $request = $this->createRequest($server);
+        $this->assertEquals($expected, $request->getSchemeAndHttpHost());
+ 
+        $server['PHP_AUTH_PW'] = 'xxxx';
+        $expected = 'http://robert:xxxx@myserver:90';
+        $request = $this->createRequest($server);
+        $this->assertEquals($expected, $request->getSchemeAndHttpHost());
+    }
+
+    /**
+     * @test
+     * @dataProvider    provideQueryStringData
+     * @return  null
+     */
+    public function normalizeQueryString($str, $expected, $msg)
+    {
+        $result = HttpRequest::normalizeQueryString($str);
+        $this->assertEquals($expected, $result, $msg);
+    }
+
+    /**
+     * @test
+     * @depends normalizeQueryString
+     * @return  null
+     */
+    public function getQueryString()
+    {
+        $server = array("QUERY_STRING" => '');
+        $request = $this->createRequest($server);
+
+        $this->assertNull(
+            $request->getQueryString(),
+            "empty strings are converted to nulls"
+        );
+    }
+
 
     /**
      * @test
@@ -488,21 +709,93 @@ class HttpRequestTest extends FrameworkTestCase
         return $host;
     }
 
+    /**
+     * @test
+     * @depends gettingTheHostTrustedProxyOnlyServerAddr
+     * @return  null
+     */
+    public function gettingTheHostCaseSensivity($host)
+    {
+        $uppercase = strtoupper($host);
+        HttpRequest::markProxyAsTrusted();
+        $server = array( 'HTTP_X_FORWARDED_HOST' => "$uppercase");
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHost());
+
+        $server = array( 'HTTP_HOST' => $uppercase);
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHost());
+
+        HttpRequest::markProxyAsUnsafe();
+        $server = array('HTTP_HOST' => $uppercase);
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHost());
+
+        $server = array('SERVER_NAME' => $uppercase);
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHost());
+
+
+        $server = array('SERVER_ADDR' => $uppercase);
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHost());
+
+        return $host;
+    }
 
     /**
      * @test
-     * @depends gettingTheHostWhenNothingIsSet
+     * @depends gettingTheHostCaseSensivity
      * @return  null
      */
-    public function gettingTheHostUnTrusted()
+    public function gettingTheHostUnTrusted($host)
     {
-        $host = 'www.example.com';
         $server = array('HTTP_HOST' => $host);
         $request = $this->createRequest($server);
         $this->assertEquals($host, $request->getHost());
+
+        return $host;
     }
 
+    /**
+     * @test
+     * @depends gettingTheHostUnTrusted
+     * @return  null
+     */
+    public function gettingTheHttpHost($host)
+    {
+        $server = array(
+            'SERVER_PORT' => '80',
+            'HTTP_HOST' => $host
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHttpHost());
 
+        $server = array(
+            'HTTPS'         => 'on',
+            'SERVER_PORT'   => '443',
+            'HTTP_HOST' => $host
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals($host, $request->getHttpHost());
+
+         $server = array(
+            'HTTPS'         => 'on',
+            'SERVER_PORT'   => '449',
+            'HTTP_HOST' => $host
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals("$host:449", $request->getHttpHost());
+
+         $server = array(
+            'SERVER_PORT'   => '89',
+            'HTTP_HOST' => $host
+        );
+        $request = $this->createRequest($server);
+        $this->assertEquals("$host:89", $request->getHttpHost());
+
+        return $host;
+    }
 
     /**
      * @test
@@ -518,6 +811,19 @@ class HttpRequestTest extends FrameworkTestCase
         $server = array('REQUEST_METHOD' => 'post');
         $request = $this->createRequest($server);
         $this->assertEquals('POST', $request->getMethod());
+    }
+
+    /**
+     * @test
+     * @depends         gettingHttpMethodNormal
+     * @dataProvider    provideIsMethodSafeData
+     * @return          null
+     */
+    public function isMethodSafe($method, $expected)
+    {
+        $server = array('REQUEST_METHOD' => $method);
+        $request = $this->createRequest($server);
+        $this->assertEquals($expected, $request->isMethodSafe());
     }
 
     /**
